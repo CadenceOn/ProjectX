5e839bcca17b5ad9c18aeb69d34822406d2a10c1
CadenceOn <113538648+CadenceOn@users.noreply.github.com>
2025-01-05 22:07:25 +0300

    NEW: Film rating

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,26 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./MovieCard.css";
+import RatingComponent from "./Rating";
 
 const MovieCard = ({
-    movie,
+  movie,
 }: {
-    movie: { nameRu: string; year: string; posterUrl: string; kinopoiskId: string };
+  movie: {
+    nameRu: string;
+    year: string;
+    posterUrl: string;
+    kinopoiskId: string;
+  };
 }) => {
-    console.log(movie);
-    return (
-        <div className="movie-card">
-            <img src={movie.posterUrl} alt={movie.nameRu} />
-            <h3>{movie.nameRu}</h3>
-            <p>{movie.year}</p>
-            <Link
-                to={`/project_x/movies/${movie.kinopoiskId}`}
-                className="details-button"
-            >
-                Посмотреть детали
-            </Link>
-        </div>
-    );
+  return (
+    <div className="movie-card">
+      <img src={movie.posterUrl} alt={movie.nameRu} />
+      <h3>{movie.nameRu}</h3>
+      <p>{movie.year}</p>
+
+      {/* Компонент оценки */}
+      <RatingComponent movieId={movie.kinopoiskId} />
+
+      <Link
+        to={`/project_x/movies/${movie.kinopoiskId}`}
+        className="details-button"
+      >
+        Посмотреть детали
+      </Link>
+    </div>
+  );
 };
 
 export default MovieCard;
