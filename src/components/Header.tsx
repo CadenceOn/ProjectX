4f56e2243f5f583684fda31edf6657dee3fc603b
Nurickkk <jahonmalikov@gmail.com>
2025-01-05 17:35:55 +0300

    NEW: back to main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import './Header.css';
 import logo from './logo.png'; // Импорт логотипа
 
 const Header = ({ onSearch }: { onSearch: (query: string) => void }) => {
     const location = useLocation();
+    const navigate = useNavigate();
 
     // Проверяем, находимся ли мы на главной странице
     const isHomePage = location.pathname === '/project_x';
 
+    // Обработчик клика по логотипу
+    const handleLogoClick = () => {
+        navigate('/project_x'); // Переход на главную страницу
+    };
+
     return (
         <header className="header">
-            {/* Добавляем логотип */}
-            <div className="logo-container">
+            {/* Логотип с обработчиком клика */}
+            <div className="logo-container" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
                 <img src={logo} alt="Logo" className="logo" />
                 <h1>Kinopoisk Clone</h1>
             </div>
