18e14a6981e2463485ae0ad25222cb5097819433
Nurickkk <jahonmalikov@gmail.com>
2025-01-05 17:18:54 +0300

    NEW: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import './Header.css';
+import logo from './logo.png'; // Импорт логотипа
 
 const Header = ({ onSearch }: { onSearch: (query: string) => void }) => {
     const location = useLocation();
@@ -11,7 +12,11 @@ const Header = ({ onSearch }: { onSearch: (query: string) => void }) => {
 
     return (
         <header className="header">
-            <h1>Kinopoisk Clone</h1>
+            {/* Добавляем логотип */}
+            <div className="logo-container">
+                <img src={logo} alt="Logo" className="logo" />
+                <h1>Kinopoisk Clone</h1>
+            </div>
             {!isHomePage && <SearchBar onSearch={onSearch} />}
         </header>
     );
